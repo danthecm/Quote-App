1fcf55cbac5400a5899d9bbde179868bde4398db
danthecm <etchiedaniel1@gmail.com>
2023-02-12 23:02:48 +0100

    feat: added quotes sorting in quotes page

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,39 @@
 import { Fragment } from 'react';
+import { useLocation, useNavigate } from "react-router-dom";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
 
 const QuoteList = (props) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const quotes = props.quotes;
+
+  const changeSortingHandler = () => {
+    navigate("/quotes?sort=" + (isAscending ? "dsc" : "asc"));
+  };
+
+  const params = new URLSearchParams(location.search);
+
+  const isAscending = params.get("sort") !== "dsc";
+  quotes.sort((a, b) => {
+    if (isAscending) {
+      return a.id > b.id ? 1 : -1;
+    } else {
+      return a.id > b.id ? -1 : 1;
+    }
+  });
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isAscending ? "Descending" : "Ascending"}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {quotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
