a751e4aae68caa286678f5742e218b7296b8886c
danthecm <etchiedaniel1@gmail.com>
2023-02-13 17:05:19 +0100

    feat: make quote details page get quotes from firebase

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet, useParams } from "react-router-dom";
 import NoQuotesFound from "../components/quotes/NoQuotesFound";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
 const QuoteDetail = () => {
   const params = useParams();
+  const { sendRequest, status, data: quote } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(params.quoteID);
+  }, [sendRequest, params]);
 
   console.log("Params: ", params);
 
-  const quote = [].find((quote) => quote.id === params.quoteID);
-  console.log("The Quote is: ", quote);
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
   if (!quote) {
     return <NoQuotesFound />;
   }
+
   return (
     <>
       <HighlightedQuote {...quote} />
